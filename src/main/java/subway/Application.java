5d7283240469ff589ba0988254e7dc182378a8d5
kkimDev <kkheentech@gmail.com>
2022-10-15 19:23:10 +0900

    initial commit

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -1,10 +1,32 @@
 package subway;
 
+import subway.domain.Line;
+import subway.domain.LineRepository;
+import subway.domain.Station;
+
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
+
+        try {
+            Line line = new Line("2호선");
+            line.addItem(new Station("잠실역"));
+            line.addItemAtIndex(0,new Station("동탄역"));
+            LineRepository.addLine(line);
+
+            for (Line liner : LineRepository.lines()){
+                for (Station stationer : liner.getItemList()){
+                    System.out.println(stationer.getName());
+                }
+            }
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,7 +1,12 @@
 package subway.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Line {
     private String name;
+
+    private List<Station> itemList = new ArrayList<>();
 
     public Line(String name) {
         this.name = name;
@@ -12,4 +17,15 @@ public class Line {
     }
 
     // 추가 기능 구현
+    public void addItemAtIndex(Integer index, Station station){
+        this.itemList.add(index, station);
+    }
+
+    public void addItem(Station station){
+        this.itemList.add(station);
+    }
+
+    public List<Station> getItemList() {
+        return itemList;
+    }
 }
