77f14019d0e57f716a9a147fd452dbce0f6e7197
kkimDev <kkheentech@gmail.com>
2022-10-18 00:47:45 +0900

    setting() commit

--- src/main/java/subway/Application.java
+++ src/main/java/subway/Application.java
@@ -4,10 +4,14 @@ import subway.domain.Line;
 import subway.domain.LineRepository;
 import subway.domain.Station;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
+
+        Map<Line, List<Station>> map = setting();
+
+
         final Scanner scanner = new Scanner(System.in);
         // TODO: 프로그램 구현
 
@@ -28,5 +32,35 @@ public class Application {
             e.printStackTrace();
         }
 
+    }
+
+    private static Map<Line, List<Station>> setting() {
+
+        Map<Line, List<Station>> map= new HashMap<>() {
+            {
+                put(new Line("2호선"),
+                        new ArrayList<>(Arrays.asList(
+                                new Station("교대역"),
+                                new Station("강남역"),
+                                new Station("역삼역")))
+                );
+                put(new Line("3호선"),
+                        new ArrayList<>(Arrays.asList(
+                                new Station("교대역"),
+                                new Station("남부터미널역"),
+                                new Station("양재역"),
+                                new Station("매봉역")))
+                );
+                put(new Line("신분당선"),
+                        new ArrayList<>(Arrays.asList(
+                                new Station("강남역"),
+                                new Station("양재역"),
+                                new Station("양재시민의숲역"))
+                        )
+                );
+
+            }
+        };
+        return map;
     }
 }
